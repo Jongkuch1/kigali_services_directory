97bcfdf4e9f09a1c5f89a8c7d01f212812336480
Jongkuch1 <jongkuch1@github.com>
2026-03-09 16:58:39 +0200

    feat: Firebase options (FlutterFire CLI) and app colour tokens

---
+++ lib/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const Color kNavy = Color(0xFF0D1B2A);
+const Color kNavyLight = Color(0xFF1A2C42);
+const Color kNavyMid = Color(0xFF162237);
+const Color kGold = Color(0xFFF5A623);
+const Color kGoldLight = Color(0xFFFFB830);
+const Color kWhite = Colors.white;
+const Color kGray = Color(0xFF8A9BB0);
+const Color kCardBg = Color(0xFF1E304A);
+const Color kBorder = Color(0xFF1E3A5F);
+const Color kBorderAccent = Color(0xFF2E4A6A);
+const Color kRed = Color(0xFFE74C3C);
+const Color kGreen = Color(0xFF2ECC71);
