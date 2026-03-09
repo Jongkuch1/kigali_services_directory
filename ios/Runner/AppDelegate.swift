021c748381cec4819d91344ee42f9a7f7f99673c
Jongkuch1 <jongkuch1@github.com>
2026-03-09 16:58:32 +0200

    chore: add Android, iOS, macOS and web platform configurations

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,29 @@
+import Flutter
+import UIKit
+import FirebaseMessaging
+
+@main
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Register for remote notifications (required for FCM on iOS)
+    UNUserNotificationCenter.current().delegate = self
+    application.registerForRemoteNotifications()
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+
+  // Pass APNs token to Firebase Messaging
+  override func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    Messaging.messaging().apnsToken = deviceToken
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+}
